3f02112a18e0e222cdc98ae558ae4d823353152d
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-08-19 21:50:06 +0200

    Ajoutez des fichiers projet.

---
+++ Unowhy Tools PE/Main.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Unowhy_Tools_PE
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lspr = new ReaLTaiizor.Controls.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.explorer = new ReaLTaiizor.Controls.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.cmd = new ReaLTaiizor.Controls.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.exit = new ReaLTaiizor.Controls.Button();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Unowhy_Tools_PE.Properties.Resources.UTPELogoW;
+            this.pictureBox1.Location = new System.Drawing.Point(12, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(258, 94);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lspr
+            // 
+            this.lspr.BackColor = System.Drawing.Color.Transparent;
+            this.lspr.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lspr.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.lspr.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lspr.Image = null;
+            this.lspr.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lspr.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.lspr.Location = new System.Drawing.Point(42, 122);
+            this.lspr.Name = "lspr";
+            this.lspr.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.lspr.Size = new System.Drawing.Size(228, 24);
+            this.lspr.TabIndex = 1;
+            this.lspr.Text = "Lazersoft Password Recovery";
+            this.lspr.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.lspr.Click += new System.EventHandler(this.lspr_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Unowhy_Tools_PE.Properties.Resources.key;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 122);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // explorer
+            // 
+            this.explorer.BackColor = System.Drawing.Color.Transparent;
+            this.explorer.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.explorer.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.explorer.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.explorer.Image = null;
+            this.explorer.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.explorer.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.explorer.Location = new System.Drawing.Point(42, 152);
+            this.explorer.Name = "explorer";
+            this.explorer.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.explorer.Size = new System.Drawing.Size(228, 24);
+            this.explorer.TabIndex = 1;
+            this.explorer.Text = "Explorer ++";
+            this.explorer.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.explorer.Click += new System.EventHandler(this.explorer_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Unowhy_Tools_PE.Properties.Resources.folder;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 152);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // cmd
+            // 
+            this.cmd.BackColor = System.Drawing.Color.Transparent;
+            this.cmd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmd.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.cmd.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmd.Image = null;
+            this.cmd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.cmd.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.cmd.Location = new System.Drawing.Point(42, 182);
+            this.cmd.Name = "cmd";
+            this.cmd.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.cmd.Size = new System.Drawing.Size(228, 24);
+            this.cmd.TabIndex = 1;
+            this.cmd.Text = "CMD";
+            this.cmd.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.cmd.Click += new System.EventHandler(this.cmd_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Unowhy_Tools_PE.Properties.Resources.cmd;
+            this.pictureBox4.Location = new System.Drawing.Point(12, 182);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.Color.Transparent;
+            this.exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exit.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.exit.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit.Image = null;
+            this.exit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.exit.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.exit.Location = new System.Drawing.Point(42, 229);
+            this.exit.Name = "exit";
+            this.exit.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.exit.Size = new System.Drawing.Size(228, 24);
+            this.exit.TabIndex = 1;
+            this.exit.Text = "Exit / Shutdown";
+            this.exit.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Unowhy_Tools_PE.Properties.Resources.shut;
+            this.pictureBox5.Location = new System.Drawing.Point(12, 229);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 2;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(9, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "by STY1001 | sty1001.wordpress.com";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(248, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "1.0";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(285, 267);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.cmd);
+            this.Controls.Add(this.explorer);
+            this.Controls.Add(this.lspr);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Unowhy Tools PE";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private ReaLTaiizor.Controls.Button lspr;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private ReaLTaiizor.Controls.Button explorer;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private ReaLTaiizor.Controls.Button cmd;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private ReaLTaiizor.Controls.Button exit;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Unowhy Tools PE/Main.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unowhy_Tools_PE
+{
+    public partial class Main : Form
+    {
+
+        //Set dark mode title bar
+
+        [DllImport("DwmApi")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, int[] attrValue, int attrSize);
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            DwmSetWindowAttribute(Handle, 19, new[] { 1 }, 4);
+            DwmSetWindowAttribute(Handle, 20, new[] { 1 }, 4);
+            DwmSetWindowAttribute(Handle, 35, new[] { 1 }, 4);
+            DwmSetWindowAttribute(Handle, 38, new[] { 1 }, 4);
+        }
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("wpeutil", "shutdown");
+        }
+
+        private void cmd_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("cmd");
+        }
+
+        private void explorer_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("epp.exe");
+        }
+
+        private void lspr_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(".\\lazesoftapp\\PasswordRecoveryApp.exe"); 
+        }
+    }
+}
